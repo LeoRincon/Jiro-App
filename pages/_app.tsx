813a669622d63f1a5b0434834f2b63a198926d7d
Leonardo Rincon <jleobotache@gmail.com>
2023-08-22 11:56:45 -0500

    feat: add Layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,10 @@
 import '@/styles/globals.css';
-import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import type { AppProps } from 'next/app';
-
-const basicTheme = createTheme({
- palette: {
-  mode: 'dark',
- },
-});
-
+import { lightTheme, darkTheme } from '../themes';
 export default function App({ Component, pageProps }: AppProps) {
  return (
-  <ThemeProvider theme={basicTheme}>
+  <ThemeProvider theme={darkTheme}>
    <CssBaseline />
    <Component {...pageProps} />
   </ThemeProvider>
