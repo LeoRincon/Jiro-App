1b4e1e080d23a56caa52b0573c4ebbf631034228
Leonardo Rincon <jleobotache@gmail.com>
2023-08-26 12:32:59 -0500

    feat: add icon dark mode

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,20 +1,30 @@
 import { useContext } from 'react';
-import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
+
+import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material';
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 
 import { UiContext } from '@/context/ui';
 
 export const Navbar = () => {
  const { handlerOpenMenu } = useContext(UiContext);
  return (
-  <AppBar position='sticky'>
-   <Toolbar>
-    <IconButton size='large' edge='start' onClick={handlerOpenMenu}>
-     <MenuOutlinedIcon />
-    </IconButton>
+  <Box sx={{ flexGrow: 1 }}>
+   <AppBar position='sticky'>
+    <Toolbar>
+     <IconButton size='large' edge='start' onClick={handlerOpenMenu}>
+      <MenuOutlinedIcon />
+     </IconButton>
 
-    <Typography variant='h5'>Jiro</Typography>
-   </Toolbar>
-  </AppBar>
+     <Typography variant='h5' component='div' sx={{ flexGrow: 1 }}>
+      Jiro
+     </Typography>
+
+     <IconButton>
+      <DarkModeOutlinedIcon />
+     </IconButton>
+    </Toolbar>
+   </AppBar>
+  </Box>
  );
 };
