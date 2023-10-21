a191d3ef0438b2d51b77d9fcfce852435115bbbf
Leonardo Rincon <jleobotache@gmail.com>
2023-10-21 12:09:11 -0500

    change: language on date

--- components/ui/EntryCard.tsx
+++ components/ui/EntryCard.tsx
@@ -10,6 +10,7 @@ import {
 
 import { UiContext } from '../../context/ui/UiContext';
 import { Entry } from '../../typings';
+import { dateFunctions } from '@/utils';
 
 interface Props {
  entry: Entry;
@@ -51,7 +52,9 @@ export const EntryCard: FC<Props> = ({ entry }) => {
     <CardActions
      sx={{ display: 'flex', justifyContent: 'end', paddingRight: 2 }}
     >
-     <Typography variant='body2'> 30 minutes ago</Typography>
+     <Typography variant='body2'>
+      {dateFunctions.getFormatDistanceToNow(entry.createdAt)}
+     </Typography>
     </CardActions>
    </CardActionArea>
   </Card>
